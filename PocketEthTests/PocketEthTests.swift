c8239423c73c8a2a9233f34eb0b2560ed17ffb3a
Luis de Leon <luyzdeleon@gmail.com>
2018-06-22 12:38:47 -0400

    Added tests

--- PocketEthTests/PocketEthTests.swift
+++ PocketEthTests/PocketEthTests.swift
@@ -7,6 +7,8 @@
 //
 
 import XCTest
+//import PocketEth
+import Pocket
 @testable import PocketEth
 
 class PocketEthTests: XCTestCase {
@@ -19,33 +21,42 @@ class PocketEthTests: XCTestCase {
         super.tearDown()
     }
     
-    // Tests for createWallet()
-    func testCreateWalletSuccess() {
-        let pk = "12345678"
-        let wallet = try? PocketEth.createWallet(privateKey: pk, data: nil)
-        XCTAssertEqual(pk, wallet?.privateKey)
-        XCTAssertEqual("ETH", wallet?.network)
+    func testWalletValidity(wallet: Wallet) {
+        XCTAssertNotNil(wallet)
+        XCTAssertNotNil(wallet.address)
+        XCTAssertNotNil(wallet.privateKey)
+        XCTAssertNotNil(wallet.network)
     }
     
-    func testCreateWalletPKError() {
-        let pk = ""
-        XCTAssertThrowsError(try? PocketEth.createWallet(privateKey: pk, data: nil))
+    // Tests for createWallet()
+    func testCreateWalletSuccess() {
+        let wallet = try? PocketEth.createWallet(data: nil)
+        testWalletValidity(wallet: wallet!)
+    }
+    
+    // Tests for importWallet()
+    func testImportWallet() {
+        let walletToImport = try? PocketEth.createWallet(data: nil)
+        let importedWallet = try? PocketEth.importWallet(privateKey: (walletToImport?.privateKey)!, address: walletToImport?.address, data: walletToImport?.data)
+        testWalletValidity(wallet: importedWallet!)
+        XCTAssertEqual(walletToImport?.privateKey, importedWallet?.privateKey)
+        XCTAssertEqual(walletToImport?.address, importedWallet?.address)
+        XCTAssertEqual(walletToImport?.network, importedWallet?.network)
     }
     
     // Tests for createTransaction()
     func testCreateTransactionSuccess() {
-        let pk = "12345678"
-        let wallet = try? PocketEth.createWallet(privateKey: pk, data: nil)
         let functionABI = "{\"constant\":false,\"inputs\":[{\"name\":\"_name\",\"type\":\"string\"},{\"name\":\"_hint\",\"type\":\"string\"},{\"name\":\"_numTokens\",\"type\":\"uint256\"},{\"name\":\"_merkleRoot\",\"type\":\"bytes32\"},{\"name\":\"_uri\",\"type\":\"string\"}],\"name\":\"createQuest\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}"
         let functionParams = ["Test Quest", "Test Hint", 10, "Test Merkle Root", "Test URI"] as [Any]
         var dataParams = [AnyHashable: Any]()
         dataParams["abi"] = functionABI
         dataParams["params"] = functionParams
-        
+
         var params = [AnyHashable : Any]()
         params["to"] = "0x0"
         params["data"] = dataParams
         
+        let wallet = try? PocketEth.createWallet(data: nil)
         let transaction = try? PocketEth.createTransaction(wallet: wallet!, params: params)
         XCTAssertNotNil(transaction)
         XCTAssertNotNil(transaction?.serializedTx)
@@ -53,21 +64,24 @@ class PocketEthTests: XCTestCase {
     }
     
     func testCreateTransactionTOError() {
-        let pk = "12345678"
-        let wallet = try? PocketEth.createWallet(privateKey: pk, data: nil)
+        let wallet = try? PocketEth.createWallet(data: nil)
         
         var params = [AnyHashable : Any]()
         params["to"] = nil
         
-        XCTAssertThrowsError(try? PocketEth.createTransaction(wallet: wallet!, params: params))
+        XCTAssertThrowsError(try PocketEth.createTransaction(wallet: wallet!, params: params))
     }
     
     // Tests for createQuery()
     func testCreateQuerySuccess() {
-        
+        let query = try? PocketEth.createQuery(params: ["rpcMethod": "eth_getTransactionCount", "rpcParams": ["0x0", "latest"]], decoder: nil)
+        XCTAssertNotNil(query)
+        XCTAssertNotNil(query?.data)
+        XCTAssertEqual(query?.network, "ETH")
+        XCTAssertNotNil(query?.decoder)
     }
     
     func testCreateQueryRPCError() {
-        
+        XCTAssertThrowsError(try PocketEth.createQuery(params: ["failedKey": "failedValue"], decoder: nil))
     }
 }
--- Podfile
+++ Podfile
@@ -5,7 +5,7 @@ target 'PocketEth' do
     # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
     use_frameworks!
 
-    pod 'Pocket', :git => 'https://github.com/pokt-network/pocket-ios-sdk.git', :branch => 'master', :commit => 'b7549298025eeec7eba4c5b770d30733a5eb60cd'
+    pod 'Pocket', :git => 'https://github.com/pokt-network/pocket-ios-sdk.git', :branch => 'master', :commit => 'bc0d02d7882f3fe935ac6fe83293a4b80a5a7a49'
     pod 'web3swift'
     pod 'CryptoSwift'
     pod 'SwiftKeychainWrapper', :git => 'git@github.com:jrendel/SwiftKeychainWrapper.git', :branch => 'develop', :commit => '77f73c354d695d976bcf1437fc9fbcea981aa2b4'
